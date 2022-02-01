b29c33cffe656432fe1845dfd9e92f4f870aaa68
fukuiretu <fukuiretu@gmail.com>
2022-02-01 11:09:32 +0900

    リファクタ

--- src/notion/client.rs
+++ src/notion/client.rs
@@ -2,7 +2,7 @@ use hyper::{Body, Method, Request, Response};
 
 pub trait Client {
     fn new(api_key: String) -> Self;
-    fn get(&self, header: String) -> Response<()>;
+    fn get(&self, header: String, url: String, body: String) -> Response<()>;
     fn post(&self, header: String) -> Response<()>;
 }
 pub struct Default {
@@ -14,7 +14,7 @@ impl Client for Default {
         Default { api_key }
     }
 
-    fn get(&self, header: String) -> Response<()> {
+    fn get(&self, header: String, url: String, body: String) -> Response<()> {
         return Response::new(());
     }
     fn post(&self, header: String) -> Response<()> {
--- src/notion/database.rs
+++ src/notion/database.rs
@@ -1,61 +1,51 @@
-use hyper::{Body, Client, Method, Request};
+use super::client;
 
 pub struct Header {
     pub name: String,
     pub value: String,
 }
 
-pub struct Database {
-    common_headers: Vec<Header>,
+pub struct Database<T: client::Client> {
+    client: T,
 }
 
-impl Database {
+impl<T: client::Client> Database<T> {
     const QUERY_BASE_URL: &'static str = "https://api.notion.com/v1/databases/{database_id}/query";
 
-    pub fn new(api_key: String) -> Database {
-        Self {
-            common_headers: vec![
-                Header {
-                    name: String::from("Authorization"),
-                    value: api_key,
-                },
-                Header {
-                    name: String::from("Notion-Version"),
-                    value: String::from("2021-08-16"),
-                },
-                Header {
-                    name: String::from("Content-Type"),
-                    value: String::from("application/json"),
-                },
-            ],
-        }
+    pub fn new(client: T) -> Database<T> {
+        Self { client }
     }
 
     #[tokio::main]
     pub async fn query(
-        self: Database,
+        self: Database<T>,
         database_id: String,
-        headers: Vec<Header>,
         body: String,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         println!("call query_catabase.");
 
-        let mut builder = Request::builder()
-            .method(Method::POST)
-            .uri(Self::QUERY_BASE_URL.replace("{database_id}", database_id.as_str()));
+        self.client.get(
+            String::from(""),
+            Self::QUERY_BASE_URL.replace("{database_id}", database_id.as_str()),
+            body,
+        );
 
-        for header in self.common_headers {
-            builder = builder.header(&header.name, &header.value);
-            println!("header.name:{}, header.value:{}", header.name, header.value);
-        }
-        for header in headers {
-            builder = builder.header(&header.name, &header.value);
-            println!("header.name:{}, header.value:{}", header.name, header.value);
-        }
-        let req = builder.body(Body::from(body))?;
+        // let mut builder = Request::builder()
+        //     .method(Method::POST)
+        //     .uri(Self::QUERY_BASE_URL.replace("{database_id}", database_id.as_str()));
 
-        let resp = Client::new().request(req).await?;
-        println!("Response: {}", resp.status());
+        // for header in self.common_headers {
+        //     builder = builder.header(&header.name, &header.value);
+        //     println!("header.name:{}, header.value:{}", header.name, header.value);
+        // }
+        // for header in headers {
+        //     builder = builder.header(&header.name, &header.value);
+        //     println!("header.name:{}, header.value:{}", header.name, header.value);
+        // }
+        // let req = builder.body(Body::from(body))?;
+
+        // let resp = Client::new().request(req).await?;
+        // println!("Response: {}", resp.status());
 
         Ok(())
     }
