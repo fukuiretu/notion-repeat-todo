2aeb812f57b540f0b3c1838acf818c4e831684a8
fukuiretu <fukuiretu@gmail.com>
2022-02-06 17:43:56 +0900

    hyperからreqwestに使用ライブラリを変更しリファクタリング

--- src/notion/client.rs
+++ src/notion/client.rs
@@ -1,9 +1,41 @@
-use hyper::{Body, Method, Request, Response};
+use reqwest;
+
+pub struct RequestHeader {
+    pub name: String,
+    pub value: String,
+}
 
 pub trait Base {
     fn new(api_key: String) -> Self;
-    fn get(&self, header: String, url: String, body: String) -> Response<()>;
-    fn post(&self, header: String) -> Response<()>;
+    fn get(
+        &self,
+        headers: Vec<RequestHeader>,
+        url: String,
+        body: String,
+    ) -> Result<(), reqwest::Error>;
+    fn post(
+        &self,
+        headers: Vec<RequestHeader>,
+        url: String,
+        data: String,
+    ) -> Result<(), Box<dyn std::error::Error>>;
+
+    fn client(&self) -> reqwest::Client {
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert(
+            "Content-Type",
+            reqwest::header::HeaderValue::from_static("application/json"),
+        );
+        headers.insert(
+            "Notion-Version",
+            reqwest::header::HeaderValue::from_static("2021-08-16"),
+        );
+
+        reqwest::Client::builder()
+            .default_headers(headers)
+            .build()
+            .unwrap()
+    }
 }
 pub struct Default {
     api_key: String,
@@ -14,10 +46,29 @@ impl Base for Default {
         Default { api_key }
     }
 
-    fn get(&self, header: String, url: String, body: String) -> Response<()> {
-        return Response::new(());
+    fn get(
+        &self,
+        headers: Vec<RequestHeader>,
+        url: String,
+        body: String,
+    ) -> Result<(), reqwest::Error> {
+        Ok(())
     }
-    fn post(&self, header: String) -> Response<()> {
-        return Response::new(());
+
+    #[tokio::main]
+    async fn post(
+        &self,
+        headers: Vec<RequestHeader>,
+        url: String,
+        data: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.client()
+            .post(url)
+            .header("Authorization", format!("Bearer '{}'", self.api_key))
+            .body(data)
+            .send()
+            .await?;
+
+        Ok(())
     }
 }
